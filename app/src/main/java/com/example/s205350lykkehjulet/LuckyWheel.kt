274bfb0dbdababb39ab276105bd957fff8a029d8
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-11-01 14:51:21 +0100

    Fixed wheel spin off by one sector

--- app/src/main/java/com/example/s205350lykkehjulet/LuckyWheel.kt
+++ app/src/main/java/com/example/s205350lykkehjulet/LuckyWheel.kt
@@ -21,10 +21,6 @@ class LuckyWheel(
         "500"
     )
 
-    // We have 22 sectors on the wheel, we divide 360 by this value to have angle for each sector
-    // we divide by 2 to have a half sector
-    private val HALF_SECTOR = 360f / 22f / 2f
-
     fun spinWheelImage() {
         degreeOld = degree % 360
         // we calculate random angle for rotation of our wheel
@@ -35,7 +31,11 @@ class LuckyWheel(
             RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f
         )
         rotateAnim.duration = 3600
+
+        //Dont reset wheel after spin
         rotateAnim.fillAfter = true
+
+        //Decelerate spin speed at end of spin
         rotateAnim.interpolator = DecelerateInterpolator()
         rotateAnim.setAnimationListener(object : Animation.AnimationListener {
             override fun onAnimationStart(animation: Animation) {
@@ -56,15 +56,21 @@ class LuckyWheel(
         var text: String? = null
         do {
             // start and end of each sector on the wheel
-            val start = HALF_SECTOR * (i * 2 + 1)
-            val end = HALF_SECTOR * (i * 2 + 3)
+            val start = WHEEL_SECTOR_DEGREES * i
+            val end = WHEEL_SECTOR_DEGREES * (i + 1)
+            i++
             if (degrees >= start && degrees < end) {
                 // degrees is in [start;end[
                 // so text is equals to sectors[i];
                 text = wheelSectors[i]
             }
-            i++
         } while (text == null && i < wheelSectors.size)
         return text
+    }
+
+
+    companion object {
+        // We have 22 sectors on the wheel, we divide 360 by this value to have angle for each sector
+        private const val WHEEL_SECTOR_DEGREES = 360f / 22f
     }
 }
