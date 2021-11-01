9bf0cbf13d13b80bc5ea948a0055369cf2b4fb04
Gabriel Christ Haugbøl <gabriel.haugbol@gmail.com>
2021-11-01 15:22:01 +0100

    LuckyWheel.kt done

--- app/src/main/java/com/example/s205350lykkehjulet/LuckyWheel.kt
+++ app/src/main/java/com/example/s205350lykkehjulet/LuckyWheel.kt
@@ -11,28 +11,21 @@ class LuckyWheel(
     private val gameFragment: GameFragment
     ) {
     private var degree = 0
-    private var degreeOld = 0
-
-    private val wheelSectors = arrayOf(
-        "Miss Turn", "600", "500", "800", "500",
-        "Bankrupt", "1500", "800", "100", "500",
-        "600", "500", "Extra Turn", "800", "500",
-        "800", "1000", "100", "300", "800", "1000",
-        "500"
-    )
 
     fun spinWheelImage() {
-        degreeOld = degree % 360
-        // we calculate random angle for rotation of our wheel
-        degree = Random.nextInt(360) + 720
-        // rotation effect on the center of the wheel
+        val degreeOld = degree % 360
+
+        //calculate random angle for rotation of our wheel + amound of wheel round trips
+        degree = Random.nextInt(360) + 1080
+
+        //Rotation effect on the center of the wheel
         val rotateAnim = RotateAnimation(
             degreeOld.toFloat(), degree.toFloat(),
             RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f
         )
         rotateAnim.duration = 3600
 
-        //Dont reset wheel after spin
+        //Don't reset wheel after spin
         rotateAnim.fillAfter = true
 
         //Decelerate spin speed at end of spin
@@ -42,12 +35,12 @@ class LuckyWheel(
             }
 
             override fun onAnimationEnd(animation: Animation) {
-                // we display the correct sector pointed by the triangle at the end of the rotate animation
+                //Pass the random wheel sector value on to the GameFragment
                 getSector(360 - degree % 360)?.let { gameFragment.continueGameAfterWheelSpin(it) }
             }
             override fun onAnimationRepeat(animation: Animation) {}
         })
-        // we start the animation
+        //Start the animation
         luckyWheelImage.startAnimation(rotateAnim)
     }
 
@@ -56,21 +49,28 @@ class LuckyWheel(
         var text: String? = null
         do {
             // start and end of each sector on the wheel
-            val start = WHEEL_SECTOR_DEGREES * i
-            val end = WHEEL_SECTOR_DEGREES * (i + 1)
-            i++
+            val start = WHEEL_SECTOR_SIZE * i
+            val end = WHEEL_SECTOR_SIZE * (i + 1)
             if (degrees >= start && degrees < end) {
                 // degrees is in [start;end[
                 // so text is equals to sectors[i];
-                text = wheelSectors[i]
+                text = WHEEL_SECTORS[i]
             }
-        } while (text == null && i < wheelSectors.size)
+            i++
+        } while (text == null && i < WHEEL_SECTORS.size)
         return text
     }
 
 
     companion object {
         // We have 22 sectors on the wheel, we divide 360 by this value to have angle for each sector
-        private const val WHEEL_SECTOR_DEGREES = 360f / 22f
+        private const val WHEEL_SECTOR_SIZE = 360f / 22f
+        private val WHEEL_SECTORS = arrayOf(
+            "Miss Turn", "600", "500", "800", "500",
+            "Bankrupt", "1500", "800", "100", "500",
+            "600", "500", "Extra Turn", "800", "500",
+            "800", "1000", "100", "300", "800", "1000",
+            "500"
+        )
     }
 }
